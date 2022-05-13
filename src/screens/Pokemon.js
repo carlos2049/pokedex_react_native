0deba730691ea2b9d5120ba9a08f7e4ca16afa44
carlos2049 <carlos.20499@gmail.com>
2022-05-13 16:22:38 -0400

    add Stats component

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -3,6 +3,7 @@ import { ScrollView } from 'react-native'
 import { getPokemonDetailsApi } from '../api/pokemon'
 import Header from '../components/Pokemon/Header'
 import Type from '../components/Pokemon/Type'
+import Stats from '../components/Pokemon/Stats'
 
 const Pokemon = ({ route: { params }, navigation }) => {
   const [pokemon, setPokemon] = useState(null)
@@ -29,6 +30,9 @@ const Pokemon = ({ route: { params }, navigation }) => {
       />
       <Type
         types={pokemon.types}
+      />
+      <Stats
+        stats={pokemon.stats}
       />
     </ScrollView>
   )
