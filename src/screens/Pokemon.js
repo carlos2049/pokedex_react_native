80297fbade91b71f85fbfea8810011d2ecc9af09
carlos2049 <carlos.20499@gmail.com>
2022-05-13 16:54:47 -0400

    add setOptions useEffect

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
 import { getPokemonDetailsApi } from '../api/pokemon'
 import Header from '../components/Pokemon/Header'
 import Type from '../components/Pokemon/Type'
@@ -7,6 +9,20 @@ import Stats from '../components/Pokemon/Stats'
 
 const Pokemon = ({ route: { params }, navigation }) => {
   const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () =>
+        <Icon
+          name='arrow-left'
+          color='#fff'
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={() => navigation.goBack()}
+        />
+    })
+  }, [navigation, params])
 
   useEffect(() => {
     (async () => {
