996c43a16e930331c64c7f6f614716381455de27
carlos2049 <carlos.20499@gmail.com>
2022-05-14 22:38:13 -0400

    add validations with formik and Yup

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,6 +1,16 @@
 import { Text, View, StyleSheet, TextInput, Button, Keyboard } from 'react-native'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const LoginForm = () => {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => console.log('formulario enviado', formValue)
+  })
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesion</Text>
@@ -8,22 +18,42 @@ const LoginForm = () => {
         placeholder='Nombre de usuario'
         style={styles.input}
         autoCapitalize='none'
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue('username', text)}
       />
       <TextInput
         placeholder='Contraseña'
         style={styles.input}
         autoCapitalize='none'
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue('password', text)}
       />
       <Button
         title='Entrar'
-        onPress={() => console.log('entrando')}
+        onPress={formik.handleSubmit} // handleSubmit activa el onSubmit de formik
       />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   )
 }
 
 export default LoginForm
+
+const initialValues = () => {
+  return {
+    username: '',
+    password: ''
+  }
+}
+
+const validationSchema = () => {
+  return {
+    username: Yup.string().required('El usuario es obligatorio'),
+    password: Yup.string().required('la constraseña es olbogatioria')
+  }
+}
 
 const styles = StyleSheet.create({
   title: {
@@ -39,5 +69,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20
   }
 })
