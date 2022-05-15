785518df57a5655ec615c07424a4b42a2bac117a
carlos2049 <carlos.20499@gmail.com>
2022-05-14 21:49:36 -0400

    add form to LoginForm

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,11 +1,43 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TextInput, Button, Keyboard } from 'react-native'
 
 const LoginForm = () => {
   return (
     <View>
-      <Text>Login form</Text>
+      <Text style={styles.title}>Iniciar sesion</Text>
+      <TextInput
+        placeholder='Nombre de usuario'
+        style={styles.input}
+        autoCapitalize='none'
+      />
+      <TextInput
+        placeholder='Contraseña'
+        style={styles.input}
+        autoCapitalize='none'
+        secureTextEntry={true}
+      />
+      <Button
+        title='Entrar'
+        onPress={() => console.log('entrando')}
+      />
     </View>
   )
 }
 
 export default LoginForm
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+})
