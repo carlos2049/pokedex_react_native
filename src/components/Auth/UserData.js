827c87b86cd225454a5551936270eccb9ac8cdb4
carlos2049 <carlos.20499@gmail.com>
2022-05-15 23:19:12 -0400

    add view and styles to UserData component

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,11 +1,62 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import useAuth from '../../hooks/useAuth'
 
 const UserData = () => {
+
+  const { auth, logout } = useAuth()
   return (
-    <View>
-      <Text>user data</Text>
+    <View style={styles.content}>
+      <View style={styles.titleBlock}>
+        <Text style={styles.title}>bienvenide</Text>
+        <Text style={styles.title}>{`${auth.firstname} ${auth.lastname}`}</Text>
+      </View>
+      <View style={styles.dataContent}>
+        <ItemMenu title="Nombre" text={`${auth.firstname} ${auth.lastname}`} />
+        <ItemMenu title="Username" text={`${auth.username}`} />
+        <ItemMenu title="Email" text={`${auth.email}`} />
+        <ItemMenu title="Total Favoritos" text={`0 pokemons`} />
+      </View>
+      <Button title='Desconectarse' onPress={logout} />
     </View>
   )
 }
 
 export default UserData
+
+const ItemMenu = (props) => {
+  const { title, text } = props
+  return (
+    <View style={styles.itemMenu}>
+      <Text style={styles.itemMenuTitle} >{title}:</Text>
+      <Text>{text}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  content: {
+    marginHorizontal: 20,
+    marginTop: 20
+  },
+  titleBlock: {
+    marginBottom: 30
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 22
+  },
+  dataContent: {
+    marginBottom: 20
+  },
+  itemMenu: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: '#cfcfcf'
+  },
+  itemMenuTitle: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+    width: 120
+  }
+})
